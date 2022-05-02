d3e845ce79d3d9e2e58a89b09d8c919dadd16c78
Nicole Ogawa <nogawa11@gmail.com>
2022-05-02 16:52:52 +0900

    added light/dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import Header from './components/Header'
-import Search from './components/Search'
-import CountryDetails from './components/CountryDetails'
+import Header from './components/Header';
+import Search from './components/Search';
+import CountryDetails from './components/CountryDetails';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = React.useState(true);
+  const changeTheme = () => {
+    setIsDarkTheme(prevState => !prevState)
+  }
+
   return (
-    <div className="App">
-      <Header />
+    <div className={isDarkTheme ? "app" : "app light"}>
+      <Header isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} changeTheme={changeTheme} />
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="country/:name" element={<CountryDetails />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className='header'>
       <Link to={"/"}>
         <h1>Where in the word?</h1>
       </Link>
-      <button className='btn-mode'>
-        <FontAwesomeIcon icon = {faMoon} /> Dark Mode
+      <button className='btn--mode' onClick={props.changeTheme}>
+        <span className='icon--mode'>{props.isDarkTheme ? <FontAwesomeIcon icon = {faSun} /> : <FontAwesomeIcon icon = {faMoon} />}</span>
+        {props.isDarkTheme ? " Light Mode" : " Dark Mode"}
       </button>
     </header>
   )
